70fdad454ab8aa8a6ce135c3013e467f5339b639
Prerona Mukherjee <preronam@Preronas-MacBook-Pro.local>
2020-12-07 19:54:34 -0600

    final

---
+++ src/resumeData.js
@@ -0,0 +1,109 @@
+let resumeData = {
+  "imagebaseurl":"https://preronam.github.io/ReactPortfolio/",
+  "name": "Prerona Mukherjee",
+  "role": "Full Stack Developer",
+  "linkedinId":"prerona24@gmail.com",
+  "roleDescription": " I have experience using the MERN stack and am eager to explore other stacks to create engaging and innovative applications.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/prerona-mukherjee-0459b3179/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/preronam",
+        "className":"fa fa-github"
+      }
+    ],
+  "aboutme":"I recently completed a Full Stack Development Certificate program at Northwestern University. I have experience in SEO content writing and am currently looking for Junior Developer positions",
+  "address":"Chicago, IL",
+
+  "education":[
+    {
+      "UniversityName":"University of Miami",
+      "specialization":"Communications and English Literature",
+      "MonthOfPassing":"May",
+      "YearOfPassing":"2015",
+    },
+    {
+      "UniversityName":"University of Chicago",
+      "specialization":"Masters in English",
+      "MonthOfPassing":"June",
+      "YearOfPassing":"2018",
+    },
+
+    {
+      "UniversityName": "Northwestern University",
+      "specialization":"Full Stack Development",
+      "MonthOfPassing":"December",
+      "YearOfPassing":"2020"
+    }
+
+    
+  ],
+  "work":[
+    {
+      "CompanyName":"Professional Theatre and Dance Youth Academy",
+      "specialization":"Managing Director",
+      "YearOfLeaving":"2018-Present"
+    },
+    {
+      "CompanyName":"The Socialist",
+      "specialization":"Digital Content Manager",
+      "MonthOfLeaving":"June",
+      "YearOfLeaving":"2017"
+    },
+
+    {
+      "CompanyName":"Roundglass",
+      "specialization":"Digital Content Writer",
+      "MonthOfLeaving":"May",
+      "YearOfLeaving":"2017",
+    }
+
+
+  ],
+  "skillsDescription":"",
+  "skills":[
+    {
+      "skillname":"Javascript"
+    },
+    {
+      "skillname":"MySQL"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Book Market",
+      "description":"Search for books and authors and buy titles securely online",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"Plant Parenthood",
+      "description":"Find plants that fit your lifestyle",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"DermPal",
+      "description":"Scan skincare products to see if they are compatible with your skin type.",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"Code Quiz",
+      "description":"Test your knowledge of core web development concepts.",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  // "testimonials":[
+  //   {
+  //     "description":"Prerona is an asset to any company.",
+  //     "name":"Some technical guy"
+  //   }
+  // ]
+}
+
+export default resumeData
